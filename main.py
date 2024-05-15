f3d1d229b248cf92b49290d2778c2a0b87981986
Radek Tesař <radek.tesar135@gmail.com>
2024-05-15 13:19:14 +0200

    test-create table

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import psycopg2
+
+def create_table():
+    conn = psycopg2.connect(
+        dbname="student",
+        user="postgres",
+        password="admin",
+        host="localhost",
+        port="5432"
+    )
+
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS teacher(
+                id SERIAL,
+                name TEXT,
+                age INT,
+                address TEXT
+    )''')
+
+    conn.commit()
+    conn.close()
+
+create_table
