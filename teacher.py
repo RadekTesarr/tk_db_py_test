ad36aec3078b87941fd0b04a61d9d7fed94d306f
Radek Tesař <radek.tesar135@gmail.com>
2024-05-15 19:25:17 +0200

    age section, address section

--- teacher.py
+++ teacher.py
@@ -11,10 +11,25 @@ root.resizable(False, False)
 label_general = tk.Label(root, text="Add data")
 label_general.grid(row=0, column=1)
 
+# ADD NAME SECTION
 label_name = tk.Label(root, text="Name: ")
 label_name.grid(row=1, column=0)
 
 entry_name = tk.Entry(root)
 entry_name.grid(row=1, column=1)
+
+# ADD AGE SECTION
+label_age = tk.Label(root, text="Age: ")
+label_age.grid(row=2, column=0)
+
+entry_age = tk.Entry(root)
+entry_age.grid(row=2, column=1)
+
+# ADD ADDRESS SECTION
+label_address = tk.Label(root, text="Address: ")
+label_address.grid(row=3, column=0)
+
+entry_address = tk.Entry(root)
+entry_address.grid(row=3, column=1)
 
 root.mainloop()
