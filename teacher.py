369a50805a73d9d660770d9b734380f2b8cb47a9
Radek Tesař <radek.tesar135@gmail.com>
2024-05-15 19:44:45 +0200

    Add data to db function

--- teacher.py
+++ teacher.py
@@ -7,6 +7,25 @@ root.title("School and database")
 root.geometry("320x280")
 root.resizable(False, False)
 
+# FUNCTIONS
+def insert_data(name, age, address):
+    conn = psycopg2.connect(
+        dbname="student",
+        user="postgres",
+        password="admin",
+        host="localhost",
+        port="5432"
+    )
+
+    # INSERT DATA COMMAND
+    cur = conn.cursor()    
+    query = ('''INSERT INTO teacher(name, age, address)
+                VALUES (%s, %s, %s)''')
+    cur.execute(query, (name, age, address))
+
+    conn.commit()
+    conn.close()
+
 # LABELS, ENTRIES
 label_general = tk.Label(root, text="Add data")
 label_general.grid(row=0, column=1)
@@ -33,7 +52,8 @@ entry_address = tk.Entry(root)
 entry_address.grid(row=3, column=1)
 
 # BUTTON
-add_button = tk.Button(root, text="Add")
+add_button = tk.Button(root, text="Add", command=lambda:insert_data(entry_name.get(), entry_age.get(), entry_address.get()))
 add_button.grid(row=4, column=1)
+
 
 root.mainloop()
