9cee7b0eab09128b5a254ae0ec976cc64f76e685
Radek Tesař <radek.tesar135@gmail.com>
2024-05-15 14:44:03 +0200

    Create GUI, basic params

---
+++ teacher.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import psycopg2
+
+'''MAIN WINDOW'''
+root = tk.Tk()
+root.title("School and database")
+root.geometry("320x280")
+root.resizable(False, False)
+
+root.mainloop()
